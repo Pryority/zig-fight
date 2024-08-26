c210641a04764c1ae56943845389f61523498864
pryority <matthewapryor@gmail.com>
2024-08-26 13:41:16 -0400

    ✨ Add Dummy to GameState and draw dummy

--- src/game.zig
+++ src/game.zig
@@ -6,6 +6,7 @@ const c = @cImport({
 const Window = @import("window.zig").Window;
 const Camera = @import("camera.zig").Camera;
 const Player = @import("player.zig").Player;
+const Dummy = @import("dummy.zig").Dummy;
 const Direction = @import("common.zig").Direction;
 const Hitbox = @import("common.zig").Hitbox;
 
@@ -35,6 +36,7 @@ pub const EnvItem = struct {
 pub const GameState = struct {
     allocator: std.mem.Allocator,
     player: Player,
+    dummy: Dummy,
     time: f32,
     camera: Camera,
     cameraDescriptions: []const []const u8,
@@ -48,12 +50,14 @@ pub const GameState = struct {
         };
 
         game_state.player = Player.init();
+        game_state.dummy = Dummy.init(@as(f32, @floatFromInt(@as(c_int, @divExact(c.GetScreenWidth(), 2)))), 0);
         game_state.camera = Camera.init(&game_state.player, @as(i32, c.GetScreenWidth()), @as(i32, c.GetScreenHeight()));
 
         std.debug.print("GameState alignment: {}\n", .{@alignOf(GameState)});
         game_state.* = GameState{
             .allocator = allocator,
             .player = game_state.player,
+            .dummy = game_state.dummy,
             .time = 0,
             .camera = game_state.camera,
             .cameraDescriptions = CAMERA_DESCRIPTIONS,
@@ -63,17 +67,24 @@ pub const GameState = struct {
     }
 };
 
-export fn gameReload(game_state_ptr: *anyopaque) void {
-    // TODO: implement
-    _ = game_state_ptr;
-}
-
 export fn gameTick(state_ptr: *anyopaque) void {
     var state = @as(*GameState, @ptrCast(@alignCast(state_ptr)));
     const delta = c.GetFrameTime();
-    state.camera.updateCenter();
+
     state.time += delta;
+    state.camera.updateCenter();
     updatePlayer(&state.player, delta, &state.envItems);
+    updateDummy(&state.dummy, delta, &state.envItems);
+
+    if (state.player.currentHitbox) |hitbox| {
+        if (state.dummy.isHit(hitbox)) {
+            state.dummy.takeDamage(10);
+            std.debug.print("Dummy hit! Health: {}", .{state.dummy.health});
+            if (state.dummy.health <= 0) {
+                state.dummy = Dummy.init(@as(f32, @floatFromInt(@as(c_int, @divExact(c.GetScreenWidth(), 2)))), 0);
+            }
+        }
+    }
 }
 
 export fn gameDraw(state_ptr: *anyopaque) void {
@@ -88,11 +99,14 @@ export fn gameDraw(state_ptr: *anyopaque) void {
     c.ClearBackground(c.RAYWHITE);
 
     // Draw Player
-    c.DrawRectangleRec(state.player.rect, c.RED);
+    // c.DrawRectangleRec(state.player.rect, c.RED);
+    state.player.draw();
+    // Draw dummy/opponent
+    state.dummy.draw();
 
     // Draw attack hitbox if it exists
     if (state.player.currentHitbox) |hitbox| {
-        c.DrawRectangleRec(hitbox, if (state.player.isCrouching) c.ORANGE else c.BLUE);
+        c.DrawRectangleRec(hitbox, if (state.player.isCrouching) c.ORANGE else c.PINK);
     }
 
     c.EndMode2D();
@@ -155,4 +169,64 @@ fn updatePlayer(player: *Player, delta: f32, envItems: *[]const EnvItem) void {
     const newPos = c.Vector2{ .x = player.position.x, .y = player.position.y };
 
     player.move(newPos);
+}
+
+fn updateDummy(dummy: *Dummy, delta: f32, envItems: *[]const EnvItem) void {
+    // Horizontal movement
+    if (c.IsKeyDown(c.KEY_LEFT)) {
+        dummy.movingDirection = Direction.LEFT;
+        dummy.position.x -= PLAYER_HOR_SPD * delta;
+    } else if (c.IsKeyDown(c.KEY_RIGHT)) {
+        dummy.movingDirection = Direction.RIGHT;
+        dummy.position.x += PLAYER_HOR_SPD * delta;
+    } else {
+        dummy.movingDirection = Direction.NEUTRAL;
+    }
+
+    // Jump
+    // dummy.handleJump();
+
+    // Crouch
+    // dummy.handleCrouch();
+
+    // Attack
+    // dummy.handleAttack(delta);
+
+    // Apply gravity
+    dummy.position.y += dummy.speed * delta;
+    dummy.speed += GRAVITY * delta;
+
+    var onGround = false;
+
+    // Collision detection
+    for (envItems.*) |envItem| {
+        if (envItem.blocking and c.CheckCollisionRecs(dummy.rect, envItem.rect)) {
+            const dummyBottom = dummy.position.y + dummy.rect.height;
+            const envItemTop = envItem.rect.y;
+
+            if (dummyBottom > envItemTop and dummy.position.y < envItemTop) {
+                // Player is landing on the top of the environment item
+                dummy.position.y = envItemTop - dummy.rect.height;
+                dummy.speed = 0;
+                onGround = true;
+                dummy.canJump = true;
+                // dummy.canCrouch = true;
+            }
+        }
+    }
+
+    // If not on the ground, dummy can't jump or crouch
+    if (!onGround) {
+        dummy.canJump = false;
+        // dummy.canCrouch = false;
+    }
+
+    const newPos = c.Vector2{ .x = dummy.position.x, .y = dummy.position.y };
+
+    dummy.move(newPos);
+}
+
+export fn gameReload(game_state_ptr: *anyopaque) void {
+    // TODO: implement
+    _ = game_state_ptr;
 }

ea37ceef57496c218738571013557802d7c0deee
pryority <matthewapryor@gmail.com>
2024-08-26 12:54:43 -0400

    🐛 Fix camera struct

--- src/camera.zig
+++ src/camera.zig
@@ -6,12 +6,12 @@ const EnvItem = @import("game.zig").EnvItem;
 
 pub const Camera = struct {
     camera: c.Camera2D,
-    player: Player,
+    player: *Player,
     width: i32,
     height: i32,
 
-    pub fn init(player: Player, width: i32, height: i32) Camera {
-        return Camera{ .camera = c.Camera2D{ .offset = c.Vector2{ .x = @floatFromInt(@divExact(c.GetScreenWidth(), 2)), .y = @floatFromInt(@divExact(c.GetScreenHeight(), 2)) }, .target = player.position, .rotation = 0.0, .zoom = 1.0 }, .player = player, .width = width, .height = height };
+    pub fn init(player: *Player, width: i32, height: i32) Camera {
+        return Camera{ .camera = c.Camera2D{ .offset = c.Vector2{ .x = @floatFromInt(@divExact(width, 2)), .y = @floatFromInt(@divExact(height, 2)) }, .target = player.position, .rotation = 0.0, .zoom = 1.0 }, .player = player, .width = width, .height = height };
     }
 
     pub fn updateCenter(self: *Camera) void {
